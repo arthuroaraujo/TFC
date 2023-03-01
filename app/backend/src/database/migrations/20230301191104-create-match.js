c56fecd46ceb5ef28ba034ee2da129915ad5af87
arthur arujo <arthurarujo@MacBook-de-Patricia.local>
2023-03-01 18:11:49 -0300

    feat: (Reqs 1, 6 e 13) Desenvolvidas as migrations e models de times, pessoas usuarias e partidas

---
+++ app/backend/src/database/migrations/20230301191104-create-match.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+     await queryInterface.createTable('matches', { 
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      homeTeamId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: "home_team_id",
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      homeTeamGoals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: "home_team_goals",
+      },
+      awayTeamId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: "away_team_id",
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      awayTeamGoals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: "away_team_goals",
+      },
+      inProgress: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        field: "in_progress",
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+  await queryInterface.dropTable('matches');
+  }
+};
---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,27 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  declare readonly id: number;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING(255),
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Team;
