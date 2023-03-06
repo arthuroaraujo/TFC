59c9ced5f45edc713b0374c9341d0ca38bf11802
arthur arujo <arthurarujo@MacBook-de-Patricia.local>
2023-03-06 15:39:00 -0300

    feat: (Req 10) Criado o middleware para que não permita o acesso com um email não cadastrado ou senha incorreta

--- app/backend/src/api/middlewares/areLoginFieldValid.ts
+++ app/backend/src/api/middlewares/areLoginFieldValid.ts
@@ -1,10 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
+import * as joi from 'joi';
 
 export default class LoginMiddleware {
   public static test(req: Request, res: Response, next: NextFunction) {
     const { email, password } = req.body;
+
     if (!email || !password) {
       return res.status(400).json({ message: 'All fields must be filled' });
+    }
+
+    const userSchema = joi.object({
+      email: joi.string().email().required(),
+      password: joi.string().min(6).required(),
+    });
+    const { error } = userSchema.validate({ email, password });
+
+    if (error) {
+      return res.status(401).json({ message: 'Invalid email or password' });
     }
     next();
   }
